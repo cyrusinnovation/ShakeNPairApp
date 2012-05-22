87b62b9ce6eb5bc15901ca0ed0fb45a71ad19cdd
Beth Brown <bbrown@cyrusinnovation.com>
2012-05-22 17:13:36 -0400

    detecting motion

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,9 +1,27 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    application.applicationSupportsShakeToEdit = true;
+    
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = HomeViewController.alloc.init
     @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
+    
+
     true
   end
+  
+  def team
+    ["Jon", "Laura", "Matt", "Moss", "Nick"]
+  end
+  
+  def current_pairing
+    @current_pairing ||= team.shuffle.each_slice(2).to_a
+  end
+  
+  def reset_pairing
+    @current_pairing = nil
+  end
+  
+
 end
--- app/home_view_controller.rb
+++ app/home_view_controller.rb
@@ -1,21 +1,42 @@
 class HomeViewController < UIViewController
   
-  
-  
-  def viewDidLoad
-    first_pair = UILabel.alloc.initWithFrame([[40,40],[200,20]])
-    first_pair.text = "Laura & Moss"
-    view.addSubview(first_pair)
-    
-    second_pair = UILabel.alloc.initWithFrame([[40,80],[200,20]])
-    second_pair.text = "Jim & Nick"
-    view.addSubview(second_pair)
+  def canBecomeFirstResponder 
+    true
+  end
 
-    third_pair = UILabel.alloc.initWithFrame([[40,120],[200,20]])
-    third_pair.text = "Jon"
-    view.addSubview(third_pair)
+  def viewDidAppear(animated)
+    super
+    self.becomeFirstResponder()
+  end
 
+  def viewWillDisappear(animated)
+    self.resignFirstResponder()
+    super
   end
   
+  def viewDidLoad
 
+    current_pairing = UIApplication.sharedApplication.delegate.current_pairing
+    
+    y = 40
+    
+    current_pairing.each do |pair|
+      view.addSubview(make_pair_label(pair, y))
+      y += 40
+    end
+  end
+  
+  def make_pair_label(pair, y)
+    label = UILabel.alloc.initWithFrame([[40,y],[200,20]])
+    label.text = (pair.size == 2) ? "#{pair.first} & #{pair.last}" : "#{pair.first}"
+    label
+  end
+
+  def motionBegan(motion, withEvent:event)
+    puts "got a motion"
+  end
+  
+  def motionEnded(motion, withEvent:event)
+    puts "got a end motion"
+  end
 end
