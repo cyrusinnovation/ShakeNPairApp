7659423d86955c757255b598831ad77609893dc3
Beth Brown <bbrown@cyrusinnovation.com>
2012-05-23 10:52:03 -0400

    animated the appearance of the pair names

--- app/home_view_controller.rb
+++ app/home_view_controller.rb
@@ -1,5 +1,7 @@
 class HomeViewController < UIViewController
-  
+
+  #framework methods
+
   def canBecomeFirstResponder 
     true
   end
@@ -13,40 +15,49 @@ class HomeViewController < UIViewController
     self.resignFirstResponder()
     super
   end
-  
+
   def viewDidLoad
     display_pairs
-    puts view.subviews
-  end
-  
-  def display_pairs
-     y = 40
-     @pair_labels = []
-
-     UIApplication.sharedApplication.delegate.current_pairing.each do |pair|
-       label = make_pair_label(pair, y)
-       @pair_labels << label
-       view.addSubview(label)
-       y += 40
-     end
-  end
-  
-  def make_pair_label(pair, y)
-    label = UILabel.alloc.initWithFrame([[40,y],[200,20]])
-    label.text = (pair.size == 2) ? "#{pair.first} & #{pair.last}" : "#{pair.first}"
-    label
   end
 
-  def redisplay_pairs
-    UIApplication.sharedApplication.delegate.reset_pairing()
-    @pair_labels.each do |pair_label|
-      pair_label.removeFromSuperview()
-    end
-    
-    display_pairs
-  end
-  
   def motionEnded(motion, withEvent:event)
-    redisplay_pairs()
+    redisplay_pairs
   end
-end
+
+  #non-framework methods
+
+  def display_pairs
+    y = 40
+    @pair_labels = []
+
+    UIApplication.sharedApplication.delegate.current_pairing.each do |pair|
+      label = make_pair_label(pair, y)
+      label.alpha = 0
+      @pair_labels << label
+      view.addSubview(label)
+      y += 40
+    end
+
+    @pair_labels.each_with_index do |label, index|
+      UIView.animateWithDuration(0.2, delay:index, options:UIViewAnimationCurveLinear, animations:lambda do
+          label.alpha = 1
+        end, completion:lambda do |finished| end)
+      end
+    end
+
+    def make_pair_label(pair, y)
+      label = UILabel.alloc.initWithFrame([[40,y],[200,20]])
+      label.text = (pair.size == 2) ? "#{pair.first} & #{pair.last}" : "#{pair.first}"
+      label
+    end
+
+    def redisplay_pairs
+      UIApplication.sharedApplication.delegate.reset_pairing()
+      @pair_labels.each do |pair_label|
+        pair_label.removeFromSuperview()
+      end
+
+      display_pairs
+    end  
+
+  end
