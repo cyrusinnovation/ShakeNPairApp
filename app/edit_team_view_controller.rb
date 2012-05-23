d1c489dfee657d4462ead73f61e4d138d60cb3c4
Beth Brown <bbrown@cyrusinnovation.com>
2012-05-23 12:16:08 -0400

    added switches

--- app/edit_team_view_controller.rb
+++ app/edit_team_view_controller.rb
@@ -2,20 +2,30 @@ class EditTeamViewController < UITableViewController
   def loadView
     self.tableView = UITableView.alloc.initWithFrame(UIScreen.mainScreen.bounds, style:UITableViewStyleGrouped)
   end
+  
 
   def numberOfSectionsInTableView(tableView)
     1
   end
   
-  def tableView(tv, numberOfRowsInSection:section)
+  def tableView(tableView, numberOfRowsInSection:section)
     UIApplication.sharedApplication.delegate.team.size
   end
   
-  def tableView(tv, cellForRowAtIndexPath:indexPath)
-    cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle,reuseIdentifier:nil)
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    cell = tableView.dequeueReusableCellWithIdentifier("team member cell")
+    cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier:"team member cell") if cell.nil?
     
     cell.textLabel.text = UIApplication.sharedApplication.delegate.team[indexPath.row]
-    cell.detailTextLabel.text = "hello"
+    inclusionSwitch = UISwitch.alloc.init
+    inclusionSwitch.setOn(true, animated: false)
+    inclusionSwitch.setTag(indexPath.row)
+    inclusionSwitch.addTarget(self, action:"switchWasSwitched:", forControlEvents:UIControlEventValueChanged)
+    cell.accessoryView = inclusionSwitch
     cell
+  end
+  
+  def switchWasSwitched(sender)
+    puts "switch was switched #{sender.tag}"
   end
 end
